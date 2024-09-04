6f408d7b061f9538696bac1c5a385c3b4e9838fb
Lyudmila <Ludmila.belenko@gmail.com>
2024-09-04 16:10:58 +0000

    case 1

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,28 @@
 // Write your code here
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  getAvailability() {
+    switch(true) {
+      case this.numCopies === 0:
+        return "out of stock";
+      case this.numCopies < 10:
+        return "low stock";
+      default:
+        return "in stock";
+    }
+  }
+
+  sell(numSold = 1) {
+    this.numCopies -= numSold;
+  }
+
+  restock(numCopies = 5) {
+    this.numCopies += numCopies;
+  }
+}
